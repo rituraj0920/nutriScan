aa7c026337ff638e8a2c0e1b54ef8c9ab7a32ddd
rituraj0920 <rituraj0920@gmail.com>
2026-02-26 09:37:46 +0530

    experiment_2

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,12 +13,12 @@ app.get("/books",(req,res)=>{
 
 if(ENV.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
-    app.get("/{*any}",(req,res)=>{
+    app.get("/{*any}",(res,req)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     });
 }
 
 
-app.listen(ENV.PORT || 10000,()=> {
+app.listen(ENV.PORT,()=> {
 console.log(`server running on port ${ENV.PORT}`)});
 
