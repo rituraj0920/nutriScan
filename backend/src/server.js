9617cfbeacd38e37383a00595a758f8af7da02a9
rituraj0920 <rituraj0920@gmail.com>
2026-02-26 11:19:16 +0530

    change in git ignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,9 +12,9 @@ app.get("/books",(req,res)=>{
 })
 
 if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
     app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"/frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
     });
 }
 
